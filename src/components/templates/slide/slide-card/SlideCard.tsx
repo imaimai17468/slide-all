4d0b8355f8346577a0929e13a3d65e4cd63a576b
imaimai17468 <tosiki_801_life@outlook.jp>
2024-05-21 21:48:35 +0900

    fix: slideの設定をActionIconでできるように修正

--- src/components/templates/slide/slide-card/SlideCard.tsx
+++ src/components/templates/slide/slide-card/SlideCard.tsx
@@ -1,6 +1,16 @@
 "use client";
 import { slideUrlAtom } from "@/atom/slideUrlAtom";
-import { Box, Button, Card, Flex, Input, Stack, Text } from "@mantine/core";
+import {
+	ActionIcon,
+	Button,
+	Card,
+	Flex,
+	Input,
+	Popover,
+	Stack,
+	Text,
+} from "@mantine/core";
+import { GearIcon } from "@radix-ui/react-icons";
 import { useAtom } from "jotai";
 import { useRef, useState } from "react";
 import { useEffect } from "react";
@@ -20,38 +30,45 @@ export const SlideCard: React.FC = () => {
 	}, [slideUrl]);
 
 	return (
-		<Card shadow="md" radius="sm" h="80%">
-			<Stack gap={16} h="100%">
-				<Flex gap={16}>
-					<Stack gap={4}>
-						<Input
-							ref={inputRef}
-							defaultValue={url ?? ""}
-							placeholder="Google Slide URL"
-						/>
-						{isError && (
-							<Text size="xs" c="red.6">
-								Invalid URL
-							</Text>
-						)}
-					</Stack>
-					<Button
-						onClick={() => {
-							if (inputRef.current && isUrl(inputRef.current.value)) {
-								const url = inputRef.current.value;
-								setUrl(url);
-								setSlideUrl(url);
-								setIsError(false);
-							} else {
-								setIsError(true);
-							}
-						}}
-					>
-						Set Slide
-					</Button>
-				</Flex>
-				<Box flex={1}>{url && <GoogleSlide url={url} />}</Box>
-			</Stack>
+		<Card shadow="md" radius="sm" h="80%" pos="relative">
+			<Popover width={300} trapFocus withArrow position="bottom" shadow="md">
+				<Popover.Target>
+					<ActionIcon bg="green.6" pos="absolute" top={8} right={8}>
+						<GearIcon />
+					</ActionIcon>
+				</Popover.Target>
+				<Popover.Dropdown>
+					<Flex gap={16}>
+						<Stack gap={4}>
+							<Input
+								ref={inputRef}
+								defaultValue={url ?? ""}
+								placeholder="Google Slide URL"
+							/>
+							{isError && (
+								<Text size="xs" c="red.6">
+									Invalid URL
+								</Text>
+							)}
+						</Stack>
+						<Button
+							onClick={() => {
+								if (inputRef.current && isUrl(inputRef.current.value)) {
+									const url = inputRef.current.value;
+									setUrl(url);
+									setSlideUrl(url);
+									setIsError(false);
+								} else {
+									setIsError(true);
+								}
+							}}
+						>
+							Set Slide
+						</Button>
+					</Flex>
+				</Popover.Dropdown>
+			</Popover>
+			{url && <GoogleSlide url={url} />}
 		</Card>
 	);
 };
